47ddc1b7749fc6e6b5e09b19676e36d40d980c2e
Andrei Sheleg <andrej.sheleg@gmail.com>
2024-08-20 16:13:28 +0300

    Add json-response

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const [city, setCity] = useState<string>('')
+ const onClickHandler = () => {
+     //const city = 'Minsk'
+     const apiKey = '1ce2c8f1225f14bf33b893989c1548cf'
+     //метод для запроса на сервер, можно исп библиотеку AXIOS
+     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+         .then(response => response.json())
+         .then(json => console.log(json))
+     //alert('onClickHandler')
+ }
+
+ const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+setCity(event.currentTarget.value)
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Weather App</h1>
+        <input type="text" value={city} onChange={onChangeHandler}/>
+        <button onClick={onClickHandler}>Get weather!</button>
     </div>
   );
 }
